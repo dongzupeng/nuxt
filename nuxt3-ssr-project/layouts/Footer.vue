fb32c8d2eda7ca34091b76d7bf4fb9a0ab0b6231
dzp <2360591410@qq.com>
2024-09-07 12:31:34 +0800

    chore:调整布局

--- nuxt3-ssr-project/layouts/Footer.vue
+++ nuxt3-ssr-project/layouts/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed bottom-0 w-full h-[80px] bg-slate-950">
+    <div class="fixed bottom-0 w-full h-[80px] bg-gray-800">
         <div class="text-white flex justify-center items-center h-full text-xs">头文字D ©{{ new Date().getFullYear() }} Created by 头文字D</div>
     </div>
 </template>
--- nuxt3-ssr-project/layouts/default.vue
+++ nuxt3-ssr-project/layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <Header />
-        <slot />
+        <div class="full-screen-minus-160 bg-white w-11/12 mt-2 m-auto p-1.5">
+            <slot />
+        </div>
         <Footer />
     </div>
 </template>
@@ -10,3 +12,8 @@ import Header from "./Header.vue";
 import Footer from "./Footer.vue";
 
 </script>
+<style>
+.full-screen-minus-160 {
+  height: calc(100vh - 180px);
+}
+</style>
